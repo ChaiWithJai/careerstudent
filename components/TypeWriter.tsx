c0b8aaf8cdd734ef5aac625fa696a37f79772638
Jay Bhagat <jaybhagat@Jays-MBP.fios-router.home>
2020-06-12 13:42:20 -0400

    styles

--- components/TypeWriter.tsx
+++ components/TypeWriter.tsx
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import Typist from "react-typist";
+import styles from "./TypeWriter.module.css"
 
 const TypeWriter: React.FC = () => {
     const [count, setCount] = useState(1);
@@ -10,7 +11,7 @@ const TypeWriter: React.FC = () => {
 
    return (
     <>{count ? <Typist avgTypingDelay={50} onTypingDone={() => setCount(0)}>
-            <span> Hey, I'm Jai</span>
+            <span className={styles.firstText}> Hey, I'm Jai</span>
             <Typist.Backspace count={12} delay={800} />
             <span> I like boxing</span>
             <Typist.Backspace count={13} delay={800} />
