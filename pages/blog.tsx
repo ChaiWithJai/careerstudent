73dbd716c53b6214ddf57f88f854f45d4bc940a7
Jay Bhagat <jaybhagat@Jays-MBP.fios-router.home>
2020-06-16 09:44:18 -0400

    blog navigation

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,8 +1,8 @@
 import Link from 'next/link'
 import Layout from '../components/Layout'
 
-const AboutPage = () => (
-  <Layout title="About | Next.js + TypeScript Example">
+const BlogPage = () => (
+  <Layout title="Career Student">
     <h1>About</h1>
     <p>This is the about page</p>
     <p>
@@ -13,4 +13,4 @@ const AboutPage = () => (
   </Layout>
 )
 
-export default AboutPage
+export default BlogPage
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,20 @@
+import Link from 'next/link'
 import Footer from '../components/Footer';
 import Layout from '../components/Layout';
+import styles from '../components/Layout.module.css';
 
 const IndexPage = () => (
-  <>
-    <Layout title="Career Student" />
-    <Footer />
-  </>
+  <div className={styles.parentContainer}>
+    <div className={styles.blogContainer}>
+      <Link href="/blog">
+        <h1 className={styles.blogText}>Blog</h1>
+      </Link>
+    </div>
+    <div className={styles.bodyAndFooterContainer}>
+      <Layout title="Career Student" />
+      <Footer />
+    </div>
+  </div>
 )
 
 export default IndexPage
